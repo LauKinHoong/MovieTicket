6cf1b4e5437d0f810e12eecb240870fd2013556f
Johnny Lee <88725535+johnnyleezh@users.noreply.github.com>
2022-08-27 18:14:37 +0800

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,125 +1,133 @@
-/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import {
   ScrollView,
-  Text,
-  View,
-  Image,
   StyleSheet,
-  TouchableOpacity
+  View
 } from 'react-native';
-import { movies } from './data';
-import { HomePage }from './HomePage';
 
+import { movies } from './data';
+import MoviePoster from './MoviePoster';
+import MoviePopup from './MoviePopup';
+
+let SQLite = require('react-native-sqlite-storage');
 export default class Movies extends Component {
+  // Add starting here
+  state = {
+    popupIsOpen: false,
+    // Day chosen by user
+    chosenDay: 0,       // choose first day by default
+    // Time chosen by user
+    chosenTime: null,
+  }
+
+  openMovie = (movie) => {
+    this.setState({
+      popupIsOpen: true,
+      movie,
+    });
+  }
+
+  closeMovie = () => {
+    this.setState({
+      popupIsOpen: false,
+      // Reset values to default ones
+      chosenDay: 0,
+      chosenTime: null,
+    });
+  }
+
+  chooseDay = (day) => {
+    this.setState({
+      chosenDay: day,
+    });
+  }
+
+  chooseTime = (time) => {
+    this.setState({
+      chosenTime: time,
+    });
+  }
+
+  bookTicket = () => {
+    // Make sure they selected time 
+    if (!this.state.chosenTime) {
+      alert('Please select show time');
+    } else {
+      // Close popup
+      this.closeMovie();
+      // Navigate away to Confirmation route
+      alert('Booking Successfully !\n\nHere is your booking no : ' + Math.random().toString(36).substring(6).toUpperCase());
+      this._insert();
+      //this.props.onPress.navigate({
+      //name: 'Confirmation',
+      // Generate random string
+      //code: Math.random().toString(36).substring(6).//toUpperCase(),
+      //});
+    }
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        email: 'johndoe@gmail.com', //change this code to get the profile's id
+      },
+      movie: [],
+    }
+    this.db = SQLite.openDatabase(
+      { name: 'movieHistory', createFromLocation: '~db.sqlite' },
+      this.openCallback,
+      this.errorCallback,
+    );
+  }
+  openCallback() {
+    console.log('database open success');
+  }
+  errorCallback(err) {
+    console.log('Error in opening the database: ' + err);
+  }
+  _insert() {
+    this.db.transaction(tx => {
+      tx.executeSql('INSERT INTO trolley(email, movieId, name, date, time, ticket, price) VALUES(?,?,?,?,?,?,?)',
+        [this.state.user.email, 1, this.state.movie.title, { chosenDay }, { chosenTime }, 1, 16])
+    });
+  }
   render() {
     return (
-        <ScrollView 
-        decelerationRate={0.6}
+      <View style={styles.container}>
+        <ScrollView
           contentContainerStyle={styles.scrollContent}
-          showsVerticalScrollIndicator={true}
-          persistentScrollbar={true}
-          
+          // Hide all scroll indicators
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
         >
-      <View style={styles.mvPage}>
-      <Text style={styles.mvHeader}>Now Showing</Text>
-      </View>
-
-          <View style={styles.aMv}>
-          
-          {movies.map((movie, index) => 
-            <View key = {movie.title}>
-              <TouchableOpacity
-            onPress={()=>{alert("you clicked me")}}
-          >
-              <Image 
-                style={styles.mvPoster} 
-                source={{uri: movie.poster}}
-              />
-              </TouchableOpacity>
-              <Text style={styles.mvTitle} numberOfLines={1} ellipsizeMode="tail">{movie.title}</Text>
-              <Text style={styles.mvGenre} numberOfLines={1} ellipsizeMode="tail">{movie.genre}</Text>
-            </View>
-          )}
-          </View>
-
-          <TouchableOpacity
-            onPress={() => this.onTestPress()}
-          >
-          </TouchableOpacity>
-         
-
+          {movies.map((movie, index) => <MoviePoster
+            movie={movie}
+            onOpen={this.openMovie}
+            key={index}
+          />)}
         </ScrollView>
-        
+        <MoviePopup
+          movie={this.state.movie}
+          isOpen={this.state.popupIsOpen}
+          onClose={this.closeMovie}
+          chosenDay={this.state.chosenDay}
+          chosenTime={this.state.chosenTime}
+          onChooseDay={this.chooseDay}
+          onChooseTime={this.chooseTime}
+          onBook={this.bookTicket}
+        />
+      </View>
     );
   }
 }
 
-onTestPress = () => {
-  this.props.navigator.push('Home');
-}
-
-const styles = StyleSheet.create ({
-  mvPage: {
-    fontSize: 25,
-  },
-
-  mvHeader: {
-    backgroundColor: 'olive',
-    textAlign: 'center',
-    fontSize: 40,
-    fontWeight: 'normal',
-    padding: 20,
-  },
-
+const styles = StyleSheet.create({
   container: {
-    paddingTop: 20,
-    marginTop: 8,
-    maxHeight: 800,
-  },
-
-  ScrollView: {
-    marginHorizontal: 20,
-  },
-
-
-  scrollContent: {
-    flexDirection: 'column', 
-    justifyContent: 'space-between',
-  },
-
-  aMv:{
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    paddingTop: 20,         // start below status bar
     backgroundColor: 'black',
   },
-
-  mvPoster: {
-    width: 120,
-    height: 200,
-    margin: 3,
-    borderRadius: 10,
-  },
-
-  mvTitle: {
-    fontSize: 18,
-    color: 'white',
-    margin: 5,
-    flexDirection:'column',
-    flex: 1,
-    flexWrap: 'wrap',
-    width: 100,
-    textAlign:'left',
-  },
-
-  mvGenre: {
-    color: '#BBBBBB',
-    fontSize: 12,
-    margin: 5,
-    textAlign:'left',
-    flexWrap: 'wrap',
-    flex: 1,
-    flexDirection:'column',
-    width: 100,
+  scrollContent: {
+    flexDirection: 'row',   // arrange posters in rows
+    flexWrap: 'wrap',       // allow multiple rows
   },
 });
